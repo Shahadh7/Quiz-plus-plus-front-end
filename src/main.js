ad1fa1adcdd997a2df35154d34c51a657f9d0b25
shahadh <shahadh.hamza@gmail.com>
2021-08-31 06:21:29 +0530

    cors header set

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+axios.defaults.headers.get['access-control-allow-origin'] = '*'
 
 new Vue({
   router,
