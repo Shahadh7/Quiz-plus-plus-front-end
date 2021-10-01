9d5e7acfa5601627e33a29688e2eb10d015fba62
shahadh <shahadh.hamza@gmail.com>
2021-10-02 01:26:44 +0530

    padding top added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,7 +37,7 @@
           </h1> 
         </v-row>
     </kinesis-container>
-    <p class="white--text text-h5 text-center ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur eius in tenetur sint cupiditate quam facere, blanditiis non ut quia a, suscipit, asperiores similique earum deleniti excepturi quis consectetur iusto cumque laudantium deserunt sapiente quibusdam. Voluptatum esse vitae iure quibusdam ipsam nemo hic tenetur quia, eum illum deserunt dolorem, ex consequatur numquam animi quas in totam rerum distinctio, minima cumque deleniti. A, tempore adipisci minima ullam maxime provident esse quod.</p>
+    <p class="white--text text-h5 text-center pt-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur eius in tenetur sint cupiditate quam facere, blanditiis non ut quia a, suscipit, asperiores similique earum deleniti excepturi quis consectetur iusto cumque laudantium deserunt sapiente quibusdam. Voluptatum esse vitae iure quibusdam ipsam nemo hic tenetur quia, eum illum deserunt dolorem, ex consequatur numquam animi quas in totam rerum distinctio, minima cumque deleniti. A, tempore adipisci minima ullam maxime provident esse quod.</p>
   
 </v-container>
 </template>
